2b572fa9d458c9bac8aef63d64e76fd71af70a36
hpratt <hpratt111@live.com>
2021-06-19 20:50:08 -0400

    skip comment lines

--- src/ld/__main__.py
+++ src/ld/__main__.py
@@ -12,7 +12,7 @@ def main(argc, argv):
     field = int(argv[4]) if argc >= 5 else 0
     batch_size = int(argv[5]) if argc >= 6 else 100
     with open(argv[2], 'r') as f:
-        snps = list({ line.strip().split()[field] for line in f })
+        snps = list({ line.strip().split()[field] for line in f if not line.startswith("#") })
     
     for percentage, batch in ld_matrix(argv[1], snps, argv[3], batch_size):
         print("%.3f%% complete\r" % percentage, file = sys.stderr)
